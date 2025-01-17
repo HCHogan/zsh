290e038c3eada81026fc689c3c2547d8648e5962
hank <ysh2291939848@outlook.com>
2025-01-18 00:05:27 +0800

    nix

--- options.zsh
+++ options.zsh
@@ -34,10 +34,10 @@ SAVEHIST=10000
 
 # brew
 # Added by OrbStack: command-line tools and integration
-source ~/.orbstack/shell/init.zsh 2>/dev/null || :
-
-  export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles #ckbrew
-  eval $(/opt/homebrew/bin/brew shellenv) #ckbrew
+# source ~/.orbstack/shell/init.zsh 2>/dev/null || :
+#
+#   export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles #ckbrew
+#   eval $(/opt/homebrew/bin/brew shellenv) #ckbrew
 
 # Autosuggestion
 ZSH_AUTOSUGGEST_USE_ASYNC="true"
