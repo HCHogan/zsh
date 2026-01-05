e40625992e772fb6762a99b38afc6d54bfca91ae
Hank Hogan <ysh2291939848@outlook.com>
2026-01-05 13:44:43 +0800

    update

--- keybinds.zsh
+++ keybinds.zsh
@@ -2,6 +2,22 @@
 ## Keybindings
 ##
 
+bindkey -v
+bindkey "^?" backward-delete-char
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+echo -ne '\e[5 q'
+
 bindkey '^[[H' beginning-of-line
 bindkey '^[[F' end-of-line
 bindkey -s '^K' 'ls^M'
